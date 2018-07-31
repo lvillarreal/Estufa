557d80da2964cfad4d2a85939250b2b27b16ab34
Luciano Villarreal <luciano95villarreal@gmail.com>
2018-07-31 02:19:49 -0300

    Tue Jul 31 02:19:49 2018

--- GitCommand.py
+++ GitCommand.py
@@ -5,7 +5,8 @@ import sys
 
 #Se obtiene la fecha y hora
 fyh = time.strftime("%c")
-print(fyh)
+
+#Se ejecutan comandos de git, para actualizar la informacion
 os.system('cd ~/Estufa')
 os.system('git add -A')
 os.system('git commit -m "%s"'%(fyh))
