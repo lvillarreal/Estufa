1cf481ae68e9ed92f7b4aa62a17f095ff8a85722
Luciano Villarreal <luciano95villarreal@gmail.com>
2018-07-31 02:17:38 -0300

    Tue Jul 31 02:17:38 2018

--- EstufaCode.py
+++ EstufaCode.py
@@ -1,7 +1,11 @@
+#!/usr/bin/python
+import time
 import os
 import sys 
 
-
+#Se obtiene la fecha y hora
+fyh = time.strftime("%c")
+print(fyh)
 os.system('cd ~/Estufa')
 os.system('git add -A')
-os.system('git status')
+os.system('git commit -m "%s"'%(fyh))
